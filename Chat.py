26d9b514d303ec2a9659759dbd356db942aaf922
Samuel Decanio <sjdecanio@gmail.com>
2017-02-26 12:09:03 -0800

    Accepts some user input from local user

--- Chat.py
+++ Chat.py
@@ -15,39 +15,63 @@ TODO:
 Layout of Interaction with user:
     User runs client
     Upon running the client
+
+
+handling of local user commands
+handling of commands/messages from other users
 """
 
 
 
 import socket
 import threading
+import sys
+import os
 
 bind_ip = "127.0.0.1"
-bind_port = 9999
+bind_port = 9997
+connections = dict()
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server.bind((bind_ip, bind_port))
-
 server.listen(100)
+
+def handle_incoming():
+    print("Incoming Handler Thread Launched.")
+    while True:
+        # we need to add the incoming connection to our list of connections
+        client, addr = server.accept()
+        print("[*] Accepted connection from: %s:%d" % (addr[0], addr[1]))
+        # ready client thread to handle incoming data
+        incoming_client_handler = threading.Thread(target=handle_incoming_client, args=(client,))
+        incoming_client_handler.start()
 
 print("[*] Listening on %s:%d" % (bind_ip, bind_port))
 
-
-#client handling thread
-def handle_client(client_socket):
+#incoming client handling thread
+def handle_incoming_client(client_socket):
     request = client_socket.recv(1024)
     print("[*] Received: %s" % request)
-
     client_socket.send("Received: %s\n" % request)
-
     client_socket.close()
 
-print("Welcome to our P2P Chat Client.\n")
-while True:
-    client, addr = server.accept()
-    print("[*] Accepted connection from: %s:%d" % (addr[0], addr[1]))
+def handle_user():
+    print("Handle User Thread Launched")
+    cont = True
+    while cont:
+        command = input(">>> ")
+        if command == 'quit':
+            quit()
 
-    #ready client thread to handle incoming data
-    client_handler = threading.Thread(target=handle_client,args=(client,))
-    client_handler.start()
+def quit():
+    os._exit(0)
+
+print("Welcome to our P2P Chat Client.\n")
+incoming_handler = threading.Thread(target=handle_incoming, args=())
+incoming_handler.start()
+user_handler = threading.Thread(target=handle_user, args=())
+user_handler.start()
+
+
+
+
